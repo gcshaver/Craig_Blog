030ff29ecb40f9fd4afc54951d07d263f927e2cb
cs <shavergc@gmail.com>
2020-01-01 11:36:47 -0500

    adding .gitignore and requirements.txt

--- craig_blog/craig_blog/urls.py
+++ craig_blog/craig_blog/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import include, url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    url('admin/', admin.site.urls),
+    url('', include('blog.urls'))
 ]
